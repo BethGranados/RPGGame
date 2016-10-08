f73a1739b8d07f13fb06c4c716fd5b11b7d353aa
BethGranados <BethGranados@gmail.com>
2016-10-08 14:30:24 -0500

    Removed outdated unused script

--- title.py
+++
@@ -1,8 +0,0 @@
-from actor import *
-
-class title(actor):
-    def __init__(self, xCord, yCord):
-        actor.__init__(self, xCord, yCord)
-        self.size = (400, 150)
-        self.image = pygame.Surface(self.size).convert()
-        self.image.blit(pygame.image.load("mainMenu.bmp"), (0,0))
